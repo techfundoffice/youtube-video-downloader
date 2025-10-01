1345580bd7bec204cb59e39765b457d2ee88a3d3
Tech Fund Office <126924774+techfundoffice@users.noreply.github.com>
2025-10-01 15:16:06 -0700

    Add frontend/vite.config.ts

---
+++ frontend/vite.config.ts
@@ -0,0 +1,32 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+      '/socket.io': {
+        target: 'http://localhost:5000',
+        ws: true,
+        changeOrigin: true,
+      },
+      '/videos': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      }
+    }
+  },
+})
